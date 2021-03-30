e60e9ca4677e833bb53eb572db06213a2afc140a
ALeksey-Filippenkov <fil_aleksey@hotmail.com>
2021-03-30 20:28:08 +0300

    First homework

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя:");
+            string name = Console.ReadLine();
+
+            Console.Write($"Привет, {name}, сегодня {DateTime.Now.ToShortDateString()}");
+            Console.ReadKey();
+        }
+    }
+}
